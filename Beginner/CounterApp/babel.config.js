a9162e1b68a20ba121af025768c8d8f8af49e66f
jadogeri <jadogeri@uno.edu>
2024-02-12 21:18:46 -0600

    added jsonconfig.json file, added alias to paths

--- Beginner/CounterApp/babel.config.js
+++ Beginner/CounterApp/babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins:[
+      [
+        'module-resolver',
+        {
+          root:['./src'],
+          alias:{
+            src: "./src",
+            '@test':  './tests',
+            '@screen': './src/screens',
+            '@component':'./src/components'
+
+          }
+
+        },
+      ]
+    ]
   };
 };
